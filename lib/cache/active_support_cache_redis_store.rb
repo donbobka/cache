745be34090bd21315871452fe27a56278bf7ae22
Vladimir Lyzo <vladimir.lyzo@saltpepper.ru>
2014-04-15 02:34:40 +0400

    Add redis-store

---
+++ lib/cache/active_support_cache_redis_store.rb
@@ -0,0 +1,10 @@
+require 'cache/active_support_cache_store'
+module Cache::ActiveSupportCacheRedisStore
+  def self.extended(base)
+    base.extend Cache::ActiveSupportCacheStore
+  end
+
+  def _stats
+    @metal.stats
+  end
+end
